48d2c05db7fe60ad27b2571667e61a6c9c19a6ae
GoncaloXavier <goncalox11@gmail.com>
2019-12-10 16:01:10 +0000

    FInished sendPayment action

--- actions/action_payment.php
+++ actions/action_payment.php
@@ -3,16 +3,6 @@
     include_once('../database/account.php');
     include_once('../database/users.php');
     
-    if(!isset($_POST['destiny']) || !isset($_POST['quantity']) ){
-        //TODO
-        //PRINT ERROR MESSAGE
-    }
-    else {
-        $destiny =$_POST['destiny'];
-        $quantity =$_POST['quantity'];
-        //TODO
-        // SQL ADD $quantity to $destiny
-    }
 
     // $origin = getAccountID($_SESSION['username']);
     if (checkIfPaymentIsPossible($_POST['quantity'], $_POST['destiny'], $origin)) {
