37c965875401cecd397c2128dd20d009cbe3d46a
GoncaloXavier <goncalox11@gmail.com>
2019-12-11 12:45:09 +0000

    Log in now verifying user

--- actions/action_login.php
+++ actions/action_login.php
@@ -1,31 +1,31 @@
 <?php 
 
-    if(!isset($_POST['username'])) {
-        header('Location: ../pages/generalview_admin.php');
-        //TODO
-        //PRINT ERROR MESSAGE
-     }
-    else{
-        //TODO
-        //CHECK IF USER IS ADMIN
-            header('Location: ../pages/generalview_admin.php');
-        // if(VERIFY USER-PASSWORD){
-            //header('Location: ../pages/generalview_user.php');
-        //}
-    }
-
-    // include_once('../includes/sessions.php');
-    // include_once('../database/user.php');
-    // include_once('../database/connection.php');
-  
-    // $username = $_POST['username'];
-    // $password = $_POST['password'];
-  
-    // if (verifyUser($username, $password)== true) {
-    //   $_SESSION['username'] = $username;
-    //   header('Location: ../pages/feed.php');
-    // } else {
-    //   header('Location: ../pages/login.php');
+    // if(!isset($_POST['username'])) {
+    //     header('Location: ../pages/generalview_admin.php');
+    //     //TODO
+    //     //PRINT ERROR MESSAGE
+    //  }
+    // else{
+    //     //TODO
+    //     //CHECK IF USER IS ADMIN
+    //         header('Location: ../pages/generalview_admin.php');
+    //     // if(VERIFY USER-PASSWORD){
+    //         //header('Location: ../pages/generalview_user.php');
+    //     //}
     // }
+
+    include_once('../includes/sessions.php');
+    include_once('../database/user.php');
+    include_once('../database/connection.php');
+  
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+  
+    if (verifyUser($username, $password)== true) {
+      $_SESSION['username'] = $username;
+      header('Location: ../pages/generalview_user.php');
+    } else {
+      header('Location: ../pages/generalview_admin.php');
+    }
 
 ?>
