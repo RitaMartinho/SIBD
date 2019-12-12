01223c364f58bb31716a72636feeeed3704ae426
RitaMartinho <rita.martinho@hotmail.com>
2019-12-12 11:25:13 +0000

    completed marge

--- database/test.php
+++ database/test.php
@@ -6,7 +6,16 @@
     include_once('account.php');
     include_once('employee.php');
     include_once('appointment.php');
-    include_once('connection.php');
+    
+
+    $db = new PDO('sqlite:bank_db.db');
+    $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db->query('PRAGMA foreign_keys = ON');
+  
+    if(NULL== $db){
+      throw new Exception("Failed to open database");
+    }
     include_once('rating.php');
     include_once('client.php');
 
